20ea01c5ec1a28d3c8fa80e6a736f770de58f4eb
sevirax <smurfacct966@gmail.com>
2025-08-20 21:06:55 +0800

    Hero Section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png';
 import cart from '../assets/cart.svg';
+import hero1 from '../assets/hero1.png'
 
-export default { logo, cart,  };
+export default { logo, cart, hero1 };
--- src/pages/Homepage/Header/header.jsx
+++ src/pages/Homepage/Header/header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './header.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../../constants';
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='app__header' id='#Home'>
+
+    <motion.div whileInView={{scale: [0.75, 1], opacity: [0, 0.5, 1]}}
+    transition={{duration: 0.5, ease: 'easeInOut'}}
+    className='app__header-info'>
+        <div className='app__content'>
+          <h1 className='headline h1-text'>More Than a Coffee, It's a Vintage</h1>
+          <p className='p-text'>Every cup is steeped in history, not just flavor. 
+          We blend tradition with taste to bring you a brew that feels timeless. 
+          This isn't just coffee—it's a nostalgic journey in every sip.</p>
+        </div>
+        <button className='app__order'>Drink Now</button>
+  </motion.div>
+  <motion.div whileInView={{scale: [0.75, 1]}}
+      transition={{duration: 0.5, ease: 'easeInOut'}}
+    className='app__hero'>
+      <img src={images.hero1} alt='Hero' className='hero'/>
+  </motion.div>
+  </div>
   )
 }
 
